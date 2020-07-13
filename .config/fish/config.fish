f19531b6ca0b9547eee26d0c96d40a7cf835159f
luis <luis@reservamos.mx>
2020-07-13 18:10:20 -0500

    Adds .config/fish/config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+alias rgrep="rg"
+alias heroku:admin="heroku browse:overview"
+alias heroku:st="heroku browse:settings"
+alias glog="git log --graph --abbrev-commit --decorate --date=relative --all"
+alias glog0="git log --graph --abbrev-commit --decorate --date=relative --all"
+alias glog1="git log --all --decorate --oneline --graph"
+
+set pure_enable_git true
+set pure_color_prompt_on_success green
+set pure_color_git_dirty "#fff700"
+set pure_color_git_branch "#ff9100"
+set pure_color_git_unpulled_commits blue
+set pure_color_git_unpushed_commits blue
+set pure_color_ssh_user_root pure_color_white
+set pure_color_ssh_user_root pure_color_light
